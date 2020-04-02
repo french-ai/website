e8c2c6a5ebc8fe016cc32650d66a2d84f3f44c16
William Jacques <46140458+aquadzn@users.noreply.github.com>
2020-04-02 17:33:02 +0200

    Update projects.js

--- src/projects.js
+++ src/projects.js
@@ -1,14 +1,15 @@
 export default {
   getProjects() {
     return [
-      // Add your project at the end in '<username>/<repo>' format!
-      'TachibanaYoshino/AnimeGAN',
-      'SkalskiP/make-sense',
+      // Add your project at the end in '<username>/<repo>', format!
       'aquadzn/gpt2-french',
       'creme-ml/creme',
       'dhaitz/mplcyberpunk',
       'french-ai/ressources',
-      "thibo73800/metacar"
+      'thibo73800/metacar',
+      '3outeille/Research-Paper-Summary',
+      'MachineLearnia/MachineLearning-tutorial',
+      'Maximellerbach/AutonomousCar',
     ];
   },
 };
